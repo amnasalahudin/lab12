712f1ca11c1777b3d9ed010b775ea0aef59b052c
amna0488 <amnasalahudin123@gmail.com>
2022-11-22 16:37:30 +0500

    first commit

--- server.test.js
+++ server.test.js
@@ -2,7 +2,11 @@ const request = require("supertest");
 const app = require('./routes/User');
 const mongoose = require('mongoose');
 
+
+
 describe("Test the root path", () => {
+
+    
     test("It should response the POST method", () => 
     {
     const response = request(app).post("http://localhost:8000/User/add"
